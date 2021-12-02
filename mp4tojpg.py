1138f8af036d8162d5d6e9e7baff45bd947c714b
sametcanvenedik <sametcanvenedik@gmail.com>
2021-12-02 23:48:35 +0300

    modified- a working version

--- mp4tojpg.py
+++ mp4tojpg.py
@@ -2,40 +2,47 @@ import cv2
 import osworks as ow
 import mymodul as md
 
-video = md.filepath("video",".mp4")
+source_list = ow.sourcepaths()
 
-cap = cv2.VideoCapture(video)
+for video in source_list:
+    
+    cap = cv2.VideoCapture(video)
 
-if cap.isOpened():
+    if cap.isOpened():
 
-    suc, folder = ow.createfolder()
-    if suc:
-        success, img = cap.read()
-        if success:
-            numofimg =0
-            winname= "Current image"
-            while True:
+        suc, folder = ow.createfolder()
+        if suc:
+            success, img = cap.read()
+            if success:
+                numofimg =0
+                winname= "Current image"
+                while True:
+
+                    end, img= cap.read()
+                    if end == False:
+                        print("Next...")
+                        print(video)
+                        print(source_list[len(source_list)-1])
+                        if video == source_list[len(source_list)-1]:
+                            cv2.putText(Limg,"Finish. press 'q' for exit.",(10,30),cv2.FONT_ITALIC,1,(255,0,0),2)
+                            cv2.destroyWindow(winname)
+                            md.cshow("Done!", cv2.cvtColor(Limg, cv2.COLOR_BGR2RGB))
+                            print("Finis")
+                            print("çıkmak  için 'q' ye basın.")
+                            md.cwait()
+
+                        break
+                    md.cshow(winname, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+                    md.cwait(1)
+                    cv2.imwrite("{}\\img{}.jpg".format(folder,numofimg),img)
+                    print("image {}".format(numofimg))
+                    numofimg+=1
+                    Limg=img
                 
-                end, img= cap.read()
-                if end == False:
-                    cv2.putText(Limg,"Finish. press 'q' for exit.",(10,30),cv2.FONT_ITALIC,1,(255,0,0),2)
-                    cv2.destroyWindow(winname)
-                    md.cshow("Done!", cv2.cvtColor(Limg, cv2.COLOR_BGR2RGB))
-
-                    break
-                md.cshow(winname, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-                md.cwait(1)
-                cv2.imwrite("{}\\img{}.jpg".format(folder,numofimg),img)
-                print("image {}".format(numofimg))
-                numofimg+=1
-                Limg=img
-            print("Finis")
-            print("çıkmak  için 'q' ye basın.")
-            md.cwait()
+            else:
+                print("videodan resim okunamadı")
         else:
-            print("videodan resim okunamadı")
-    else:
-        print("Dosya oluşturulamadı!")
+            print("Dosya oluşturulamadı!")
 
-else:
-    print("Video bulunamadı!")
+    else:
+        print("Video bulunamadı!")
--- osworks.py
+++ osworks.py
@@ -1,6 +1,16 @@
 import os
 import mymodul as md
 
+def sourcepathgenerator():
+    spath= md.filepath("","")
+    sname= "video"
+    source_num =0
+    while True:
+        source = spath + sname + "{}".format(source_num) + ".mp4"
+        source_num += 1
+        yield source
+
+
 def pathgenetator():
     fpath = md.filepath("npsource","")
     fname = "images"
@@ -20,4 +30,15 @@ def createfolder():
     
     return os.path.exists(folder), folder
 
+def sourcepaths():
+    paths =[]
+    path = sourcepathgenerator()
+    source = next(path)
+    while os.path.exists(source):
+        paths.append(source)
+        source = next(path)
+    return paths
+
+def test():
+    print(sourcepaths())
 
