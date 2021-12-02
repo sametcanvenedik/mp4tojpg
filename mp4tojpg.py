285fb30811f5636ad017da2890721d85ab244ebd
sametcanvenedik <sametcanvenedik@gmail.com>
2021-12-02 22:02:39 +0300

    a working version

--- mp4tojpg.py
+++ mp4tojpg.py
@@ -6,8 +6,36 @@ video = md.filepath("video",".mp4")
 
 cap = cv2.VideoCapture(video)
 
-if cap.isOpened() ==  False:
+if cap.isOpened():
 
-    folder = ow.createfolder()
+    suc, folder = ow.createfolder()
+    if suc:
+        success, img = cap.read()
+        if success:
+            numofimg =0
+            winname= "Current image"
+            while True:
+                
+                end, img= cap.read()
+                if end == False:
+                    cv2.putText(Limg,"Finish. press 'q' for exit.",(10,30),cv2.FONT_ITALIC,1,(255,0,0),2)
+                    cv2.destroyWindow(winname)
+                    md.cshow("Done!", cv2.cvtColor(Limg, cv2.COLOR_BGR2RGB))
 
-    
+                    break
+                md.cshow(winname, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+                md.cwait(1)
+                cv2.imwrite("{}\\img{}.jpg".format(folder,numofimg),img)
+                print("image {}".format(numofimg))
+                numofimg+=1
+                Limg=img
+            print("Finis")
+            print("çıkmak  için 'q' ye basın.")
+            md.cwait()
+        else:
+            print("videodan resim okunamadı")
+    else:
+        print("Dosya oluşturulamadı!")
+
+else:
+    print("Video bulunamadı!")
--- osworks.py
+++ osworks.py
@@ -1,18 +1,23 @@
 import os
 import mymodul as md
 
-fpath = md.filepath("npsource","")
-fname = "images"
+def pathgenetator():
+    fpath = md.filepath("npsource","")
+    fname = "images"
+    file_num = 0
+    while True:
+        folder = fpath+"\\" + fname +str(file_num)
+        file_num += 1
+        yield folder
 
-global file_num
 
 def createfolder():
-    file_num = 0
-    while os.path.exists(fpath+"\\" + fname +str(file_num)): # generator kullanıla bilirmi bi bakalım
-        file_num += 1
-        folder = fpath+"\\" + fname +str(file_num)
+    path = pathgenetator()
+    folder = next(path)
+    while os.path.exists(folder):
+        folder = next(path)
     os.makedirs(folder)
-    return folder
-
+    
+    return os.path.exists(folder), folder
 
 
